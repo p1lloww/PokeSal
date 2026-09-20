8133f2f9d9e547ebaa15bb9ab94d2811b608c593
Robson Santos <santosrobson635@gmail.com>
2026-09-20 18:02:16 -0300

    feat: add ElementType enum

---
+++ src/main/java/br/edu/ucsal/tqs/pokesal/entities/ElementType.java
@@ -0,0 +1,10 @@
+package br.edu.ucsal.tqs.pokesal.entities;
+
+/**
+ * Tipo elemental de um PokeSal ou golpe, usado no calculo de vantagem de tipo.
+ */
+public enum ElementType {
+    FIRE,
+    WATER,
+    PLANT
+}
