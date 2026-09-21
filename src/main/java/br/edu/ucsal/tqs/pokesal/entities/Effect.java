83efccf76506a2ee9d091aa808d153e2ef6107b5
Robson Santos <santosrobson635@gmail.com>
2026-09-20 21:05:54 -0300

    style: fix checkstyle indentation in Effect

--- src/main/java/br/edu/ucsal/tqs/pokesal/entities/Effect.java
+++ src/main/java/br/edu/ucsal/tqs/pokesal/entities/Effect.java
@@ -8,25 +8,30 @@ package br.edu.ucsal.tqs.pokesal.entities;
  * próprio; os demais métodos herdam o comportamento neutro declarado nesta interface.
  */
 public interface Effect {
-    /**
-     * Aplica este efeito ao PokeSal informado. É sempre invocado exatamente uma vez, no momento em que o item correspondente é utilizado.
-     *
-     * @param pokeSal o PokeSal que recebe o efeito
-     */
-    void applyEffect(PokeSal pokeSal);
 
-    /**
-     * Reverte este efeito no PokeSal informado. É invocado apenas para efeitos temporários, exatamente uma vez, no turno em que a expiração está agendada.
-     *
-     * @param pokeSal o PokeSal do qual o efeito é revertido
-     */
-    default void removeEffect(PokeSal pokeSal) {}
+  /**
+   * Aplica este efeito ao PokeSal informado. É sempre invocado exatamente uma vez,
+   * no momento em que o item correspondente é utilizado.
+   *
+   * @param pokeSal o PokeSal que recebe o efeito
+   */
+  void applyEffect(PokeSal pokeSal);
 
-    /**
-     * Indica se este efeito é temporário e deve ter sua reversão agendada ao ser aplicado.
-     * @return true se o efeito é temporário e será revertido posteriormente; false se o efeito é instantâneo e permanente
-     */
-    default boolean isTemporary() {
-        return false;
-    }
+  /**
+   * Reverte este efeito no PokeSal informado. É invocado apenas para efeitos
+   * temporários, exatamente uma vez, no turno em que a expiração está agendada.
+   *
+   * @param pokeSal o PokeSal do qual o efeito é revertido
+   */
+  default void removeEffect(PokeSal pokeSal) {}
+
+  /**
+   * Indica se este efeito é temporário e deve ter sua reversão agendada ao ser aplicado.
+   *
+   * @return true se o efeito é temporário e será revertido posteriormente;
+   *     false se o efeito é instantâneo e permanente
+   */
+  default boolean isTemporary() {
+    return false;
+  }
 }
