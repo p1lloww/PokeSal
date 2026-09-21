56f2273bc75ecda5efbef8fb0541c8916026248d
Robson Santos <santosrobson635@gmail.com>
2026-09-21 03:17:51 -0300

    feat: add MoveAction entity

---
+++ src/main/java/br/edu/ucsal/tqs/pokesal/entities/turnactions/MoveAction.java
@@ -0,0 +1,42 @@
+package br.edu.ucsal.tqs.pokesal.entities.turnactions;
+
+import br.edu.ucsal.tqs.pokesal.entities.Move;
+import br.edu.ucsal.tqs.pokesal.entities.PokeSal;
+import br.edu.ucsal.tqs.pokesal.entities.battlegrounds.Battleground;
+import br.edu.ucsal.tqs.pokesal.services.DamageCalculatorService;
+import java.util.Random;
+
+/**
+ * Ação de turno que representa o uso de um golpe (Move) por um PokeSal, aplicando
+ * dano ao oponente e, com a chance definida pelo golpe, uma condição de status.
+ */
+public class MoveAction implements TurnAction {
+
+  private static final Random RANDOM = new Random();
+
+  private final Move move;
+
+  /**
+   * Cria uma nova ação de ataque com o golpe informado.
+   *
+   * @param move o golpe a ser executado
+   * @throws NullPointerException se move for nulo
+   */
+  public MoveAction(Move move) {
+    if (move == null) {
+      throw new NullPointerException("O golpe não pode ser nulo");
+    }
+
+    this.move = move;
+  }
+
+  @Override
+  public void execute(PokeSal actor, PokeSal opponent, Battleground battleground) {
+    double damage = DamageCalculatorService.calculateDamage(actor, opponent, move, battleground);
+    opponent.takeDamage(damage);
+
+    if (move.statusEffect() != null && RANDOM.nextDouble() < move.statusChance()) {
+      opponent.applyStatus(move.statusEffect());
+    }
+  }
+}
